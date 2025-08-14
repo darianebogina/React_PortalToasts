ff02bab2ee7aa42f78474966a8d34833ebe3f0f3
Daria <dasha.nebogina@mail.ru>
2025-08-14 23:17:27 +0300

    feat: implement list of toasts

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,23 +1,17 @@
-import {useEffect, useState} from 'react'
+import {useState} from 'react'
 import {createPortal} from 'react-dom';
 import './App.css'
 
-export function App() {
-    const [show, setShow] = useState(false);
-    useEffect(() => {
-        if (show) {
-            const timer = setTimeout(() => {
-                setShow(false);
-            }, 1300);
-            return (() => clearTimeout(timer));
-        }
-    }, [show]);
+type Toast = {
+    text: string;
+    id: number;
+}
 
+export function App() {
     return (
         <>
             <Counter/>
-            {show ? createPortal(<Modal/>, document.body) : null}
-            <button onClick={() => setShow(true)}></button>
+            <ToastPanel/>
         </>
     )
 }
@@ -26,21 +20,58 @@ const Counter = () => {
     const [count, setCount] = useState(0);
     return (
         <>
-            <h1>COUnTer</h1>
+            <h1>CoUnTeRCoUnTer</h1>
             <button onClick={() => setCount((count) => count + 1)}>
                 count is {count}
             </button>
-            <h3>Toasts here</h3>
         </>
     )
 }
 
-const Modal = () => {
+const ToastPanel = () => {
+    const [items, setItems] = useState<Array<Toast>>([]);
+
+    const addNewToast = () => {
+        const item = {
+            text: "I'm a new toast!",
+            id: Math.floor(Math.random() * 10000),
+        }
+        setItems((prev) => [item, ...prev]);
+        deleteNewToast(item.id);
+    }
+
+    const deleteNewToast = (id: number) => {
+        setTimeout(() => {
+            setItems(prev => prev.filter((item: Toast) => item.id !== id));
+        }, 2000);
+    }
 
     return (
+        <>
+            <h3>Toasts here</h3>
+            {createPortal(<List items={items}/>, document.body)}
+            <button onClick={addNewToast}>Toast</button>
+        </>
+    )
+};
+
+const List = ({items}: {items: Array<Toast>}) => {
+    return (
         <div className="modal">
-            <p>I'm Modal</p>
+            <ul>
+                {items.map((item: Toast) => (
+                    <ListItem text={item.text} key={item.id}/>
+                ))}
+            </ul>
         </div>
     )
+}
+
+const ListItem = ({text}: { text: string }) => {
+    return (
+        <li>
+            {text}
+        </li>
+    );
 }
 
